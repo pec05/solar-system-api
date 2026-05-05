83a0e33ea145428922baa7d279e442c9d50f8e00
pec05 <leandro-peccio@hotmail.com>
2026-05-05 12:14:50 +0200

    Spring Boot API for Solar System 3D Viewer

---
+++ src/main/java/com/peccio/solar_system_api/SolarSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.peccio.solar_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarSystemApiApplication.class, args);
+	}
+
+}
